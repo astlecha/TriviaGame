871a51edda79a7104b7fc7e369a76fd15967c742
Charlotte Astle <charlotteastle@Charlottes-MBP.local.tld>
2017-06-20 16:16:40 -0700

    adds pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+
+var numberCorrect = 0;
+var numberIncorrect = 0;
+var unanswered = 0;
+
+var questions = ['','','','','','','','','',''];
+
+//make a function for the WHOLEgame. trigger on START click.
+//make a function for the question loop
+//for loop
+
+//show question and options in question box for 30 seconds
+	
+	//if 
+
+		//timer runs out
+			//show message "you ran out of time!!" for 5 seconds
+			//go on to next question
+
+		//user answers, 
+			//if question is correct, 
+				//congratulate for 5 seconds
+				//increment numberCorrect
+				//go to next question
+			//if question is incorrect,
+				//notify
+				//increment numberIncorrect
+				//go to next question
+//close the question loop function
+
+
+//create the reset game function. set numberCorrect, numberIncorrect, and unanswered to 0.
+	//call question loop function
+
+
+//end of questions function. Shows correct answers, incorrect answers, unanswered.
+	//call reset game function
+
+//close the WHOLEgame function.
