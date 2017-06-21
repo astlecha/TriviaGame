b633d5ff05a849680123973e33daa86bc5ad1a8c
Charlotte Astle <charlotteastle@Charlottes-MacBook-Pro.local>
2017-06-21 15:12:43 -0700

    adds css styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,55 @@
 var numberCorrect = 0;
 var numberIncorrect = 0;
 var unanswered = 0;
+var newQuestion = $("#question-box");
 
-var questions = {
-	q1: a1,
-	q2: a2,
+var objQuestions = {
+	q1 : 'Who wrote To Kill a Mockingbird?',
+
+	q2 : 'Who wrote The Great Gatsby?',
+
+	q3 : 'Who wrote "The Adventures of Huckleberry Finn"?',
+
+	};
+
+var objAnswers = {
+	a1 : ['Harper Lee', 'F. Scott Fitzgerald', 'Ernest Hemingway'],
+	a2 : ['Person 1', 'Person 2', 'Person 3',],
+
 };
 
-//make a function for the WHOLEgame. trigger on START click.
+
 //make a function for hiding question and answers
+function hideItem(htmlItem){
+	$(htmlItem).hide();
+};
+
+
+//make a function for the WHOLEgame. trigger on START click.
+$('#start-button').click(function(){
+
+hideItem('#start-button');
+
+// $('#demo').click(timedText());
+
+// function timedText() {
+//     setTimeout(timeQuestion1, 2000) 
+//     setTimeout(timeQuestion2, 4000)  
+// };
+
+// function timeQuestion1() {
+//     $('#demo').append = "QUESTION 1";
+// };
+// function timeQuestion2() {
+//     $('#demo').append = "QUESTION 2";
+// };
 
 //make a function for the question object
 //for loop
 
-//show question & options in question box 
+// iterateObject(questions);
+
+//show question & options in question box
 	//and set time remaining to 30 seconds with countdown
 	
 	//if 
@@ -22,6 +58,7 @@ var questions = {
 		//timer runs out
 			//hide question&answers
 			//show message "you ran out of time!!" for 5 seconds
+				// newQuestion.append('You ran out of time!');
 			//increment unanswered
 			//go on to next question
 
@@ -30,11 +67,13 @@ var questions = {
 			//if option = answer,
 				//hide question&answers
 				//show message "Correct answer!" for 5 seconds
+					// newQuestion.append('Correct answer!');
 				//increment numberCorrect
 				//go to next question
 			//else if option != answer,
 				//hide question&answers
-				//show message "Wrong answer!" & the correct answer for 5 seconds
+				//show message "Incorrect answer!" & the correct answer for 5 seconds
+					// newQuestion.append('Incorrect answer!');
 				//increment numberIncorrect
 				//go to next question
 //close the question object function
@@ -48,7 +87,10 @@ var questions = {
 
 //end of questions function. 
 	//hide question&answers
+
 	//Show correct answers, incorrect answers, unanswered.
+
 	//call reset game function
 
 //close the WHOLEgame function.
+})
