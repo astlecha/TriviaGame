a8a566b9b65011d8f012b18c696e887e4a7a6d64
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-06-22 22:53:20 -0700

    refines CSS and reorganizes js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,8 @@
-
 var numberCorrect = 0;
 var numberIncorrect = 0;
 var unanswered = 0;
 var questionCount = 0;
-var delayButtonAlert;
-var countdown = 30;
+var countdown = 21;
 
 var questionObject = {
 	"ques" : [
@@ -32,20 +30,65 @@ var questionObject = {
 			"answer" : "Beth March",
 			"choices" : ["Amy March", "Meg March", "Jo March", "Beth March"],
 			},
+
+		{ "question" : "Which book did Hemingway publish most recently?",
+			"answer" : "The Old Man and the Sea",
+			"choices" : ["The Old Man and the Sea", "The Sun Also Rises", "The Garden of Eden", "A Farewell to Arms"],
+			},
+
+		{ "question" : "Which book was a bestseller in 2016?",
+			"answer" : "milk and honey",
+			"choices" : ["Mockingjay", "Before I Fall", "Bossypants", "milk and honey"],
+			},
+
+		{ "question" : "What language was Aeneid by Virgil written in?",
+			"answer" : "Latin",
+			"choices" : ["Greek", "Italian", "Latin", "Lithuanian"],
+			},
+
+		{ "question" : "How many lines of dactylic hexameter are there in The Odyssey?",
+			"answer" : "12,110",
+			"choices" : ["220", "12,110", "7,840", "1,522"],
+			},
+
+		{ "question" : "What Shakespeare play is this line from:'To thine own self be true'?",
+			"answer" : "Hamlet",
+			"choices" : ["Romeo & Juliet", "Julius Caesar", "Hamlet", "Macbeth"],
+			},
 		],
 	};
 
 
 $("#answer-box").hide();
-$('results-box').hide();
+$('#results-box').hide();
+$('#wrong-answer').hide();
+$('#right-answer').hide();
+
+//START THE GAME
+//start button click triggers begin function
+$("#start-button").on("click", function() {
+		$("#start-button").hide();
+
+	numberCorrect = 0, numberIncorrect = 0, unanswered = 0, questionCount=0, countdown = 21;
+
+	countInterval = setInterval(nextQuestionCountDown, 1000);
+
+	begin();
+
+});
+
 
 //show question, start countdown
 function askQuestion(questionCount) {
-	countDownTimer=31;
+	$('#wrong-answer').hide();
+	$('#right-answer').hide();
+	countdown = 21;
+	console.log(countdown);
 	console.log("You're on question # " + questionCount);
-	// var countdown = 30;
+
 	$('#answer-box').show();
-	if( questionCount <= 5 ) {
+
+	if( questionCount < 10 ) {
 		console.log("Question displayed: " + questionObject.ques[questionCount].question);
 		$('#question-box').html(questionObject.ques[questionCount].question);
 
@@ -58,81 +101,115 @@ function askQuestion(questionCount) {
 
 	else {
 		//start the questions over, show results function
-		clearInterval(countdownIncrement);
+		clearInterval(countInterval);
 		results();
 	}
 }
 
 
-function nextQuestionCount() {
-	countDownTimer--;
+function nextQuestionCountDown() {
+	countdown--;
 
-    $('#countdown').html('<h2>Time Remaining: ' + countDownTimer + ' seconds</h2>');
+    $('#countdown').html('<h2>Time Remaining: ' + countdown + ' seconds</h2>');
+    
+    if (countdown === 0){
 
-
-    if (countDownTimer === 0){
-
-        clearInterval(countdownIncrement);
+        clearInterval(countInterval);
 
         unanswered++;
         console.log("Unanswered Questions: " + unanswered);
-        console.log('Time Up!')
+        alert('Time Up!');
 
         questionCount++;
 
-        if ( questionCount == 5 ) {
-        	clearInterval(countdownIncrement);
+        if ( questionCount == 10 ) {
+        	clearInterval(countInterval);
         	results();
         }
         else {
 	        askQuestion(questionCount);
-	        countDownTimer = 30;
-	        countdownIncrement = setInterval(nextQuestionCount,1000);
+	        countdown = 21;
+	        countInterval = setInterval(nextQuestionCountDown,1000);
         }
+
     }
 }
 
+
 //check if answer is correct
 function checkAnswer(selected) {
-	if( selected === questionObject.ques[questionCount].answer) {
+	if(selected === questionObject.ques[questionCount].answer) {
 		return true;
 	}
-
 	else {
 		return false;
 	}
 }
 
+//================I couldn't get these to work properly with the questionCount===========
+// function wrongAnswer(){
+// 	$('#question-box').hide();
+// 	$('#answer-box').hide();
+// 	$('#countdown').hide();
+// 	numberIncorrect++;
+// 	console.log("Number incorrect: "+numberIncorrect);
+// 	questionCount++;
+// 	$('#wrong-answer').show();
+// 	setTimeout(askQuestion(questionCount), 5000);
+// }
+
+// function rightAnswer(){
+// 	$('#question-box').hide();
+// 	$('#answer-box').hide();
+// 	$('#countdown').hide();
+// 	numberCorrect++;
+// 	console.log("Number correct: "+numberCorrect);
+// 	questionCount++;
+// 	$('#right-answer').show();
+// 	setTimeout(askQuestion(questionCount), 5000);
+// }
+
 //function that starts the entire game
 function begin(){
 	
+	$('#wrong-answer').hide();
+	$('#right-answer').hide();
+	$('#results-box').hide();
+	$('#question-box').show();
+	$('#answer-box').show();
+	$('#countdown').show();
 	//ask the question
 	askQuestion(questionCount);
 
-	//set a 1 second interval for nextQuestionCount function
-	countdownIncrement = setInterval(nextQuestionCount,1000);
+	counter=21;
 
 	//if an item is clicked...
 	$('.list-group-item').on('click', function(){
 
 		//if it's correct
 		if (checkAnswer($(this).html()) === true){
-				numberCorrect++;
-				console.log("Number correct: "+numberCorrect);
-				questionCount++;
-				askQuestion(questionCount);
+				// rightAnswer();
+			alert("Good job! You selected the correct answer!")
+			numberCorrect++;
+			console.log("Number correct: "+numberCorrect);
+			questionCount++;
+			askQuestion(questionCount);
+
 		}
 
 		//else if it's incorrect
 		else if (checkAnswer($(this).html()) === false){
-				numberIncorrect++;
-				console.log("Number incorrect: "+numberIncorrect);
-				questionCount++;
-				askQuestion(questionCount);
-		}
+				//wrongAnswer();
+			alert("Wrong answer :( Better luck next time!")
+			numberIncorrect++;
+			console.log("Number incorrect: "+numberIncorrect);
+			questionCount++;
+			askQuestion(questionCount);
+
+		};
 	})
 
-	if (countDownTimer === 0){
+	if (countdown === 0){
 		$('#question-box').hide();
 		$('#answer-box').hide();
 		$('#results-box').html("You ran out of time!");
@@ -142,44 +219,27 @@ function begin(){
 }
 
 
-//START THE GAME
-//start button click triggers begin function
-$("#start-button").on("click", function() {
-		$("#start-button").hide();
+function results () {
+	$('#question-box').hide();
+	$('#answer-box').hide();
+	$('#countdown').hide();
+	$('#results-box').show();
+	$('#correct').html("Number Correct: " + numberCorrect);
+	$('#incorrect').html("Number Incorrect: " + numberIncorrect);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#play-again').on("click", function(){
+		resetGame();
+		begin();
+	});
 
-	numberCorrect = 0, numberIncorrect = 0, unanswered = 0, questionCount=0, countDownTimer = 30;
+	console.log("Number Correct: " + numberCorrect);
+	console.log("Number Incorrect: " + numberIncorrect);
+	console.log("Unanswered: " + unanswered);
 
-	begin();
-
-		// if (countDownTimer === 0){
-		// 	$('#question-box').hide();
-		// 	$('#answer-box').hide();
-		// 	$('#results-box').html("You ran out of time!");
-		// 	unanswered++;
-		// 	askQuestion();
-		// }
-
-});
-
-
-//show question & options in question box
-	//and set time remaining to 30 seconds with countdown
-	
-	// if 
+}
 
 
+function resetGame(){
+	numberCorrect = 0, numberIncorrect = 0, unanswered = 0, questionCount=0, countdown = 21;
 
-//create the reset game function.
-	//reset time remaining to 30seconds
-	//set numberCorrect, numberIncorrect, and unanswered to 0.
-	//call question object function
-
-
-//end of questions function. 
-	//hide question&answers
-
-	//Show correct answers, incorrect answers, unanswered.
-
-	//call reset game function
-
-//close the WHOLEgame function.
+}
